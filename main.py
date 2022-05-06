84fc00a2e5cfd614d6d05da1342f22877bcb63eb
dexmcmillan <dexmcmillan@gmail.com>
2022-05-06 15:45:28 -0400

    fixed geckodriver issue maybe?

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 import datetime as dt
 from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
 
 options = Options()
 options.headless = True
 
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
 today = dt.datetime.today().strftime("%Y-%m-%d_%H-%M")
 
-browser = webdriver.Firefox(executable_path="geckodriver.exe", options=options)
+browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
 browser.get("http://www.cbcnews.ca")
 browser.save_screenshot(f'screenshots/{today}.png')
 browser.quit()
