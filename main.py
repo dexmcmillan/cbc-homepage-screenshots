f9fc6f8bc962fd8e44d351506d76b8297955791e
dexmcmillan <dexmcmillan@gmail.com>
2022-05-07 12:47:23 -0400

    changed screenshot window

--- main.py
+++ main.py
@@ -9,10 +9,20 @@ options.headless = True
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
+options.add_argument('--start-fullscreen')
+options.add_argument("--start-maximized")
+options.add_argument("--window-size=1080,1080")
 
 today = dt.datetime.today().strftime("%Y-%m-%d_%H-%M")
 
 browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
 browser.get("http://www.cbcnews.ca")
-browser.save_screenshot(f'screenshots/{today}.png')
+
+# get element 
+featured = browser.find_element_by_class_name("featuredNewsContentPackage")
+sidebar = browser.find_element_by_class_name("trendingList-sidebar")
+  
+# click screenshot 
+featured.screenshot(f'screenshots/{today}-featured.png')
+sidebar.screenshot(f'screenshots/{today}-sidebar.png')
 browser.quit()
